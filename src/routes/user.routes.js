1f468018aa7a98d5e80290c2ff05ba17719f888d
unknown <khushisingh74003@gmail.com>
2025-07-08 14:21:27 +0530

    added register controller and bug fixes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,17 @@
 import express from "express";
 import { registerUser } from "../controllers/user.controller.js";
-
+import { upload } from "../middlewares/multer.middleware.js";
 const router = express.Router();
 
-router.post("/register", registerUser); // 👈 Defines POST /register
+router.post("/register", upload.fields([
+     {
+        name: "avatar",  //injecting middleware
+        maxCount: 1
+     },
+     {
+        name: "coverImage",
+        maxCount: 1
+     }
+]), registerUser); // 👈 Defines POST /register
 
 export default router;
