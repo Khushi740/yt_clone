b795ca05e7ea1b42797796b6aa0bebf9f6dd181c
unknown <khushisingh74003@gmail.com>
2025-07-27 20:06:08 +0530

    added publish video functionality

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -5,6 +5,7 @@ import { User } from "../models/user.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 export const getAllVideos = asyncHandler(async (req, res) => {
     const { page = 1, limit = 10, query, sortBy = "createdAt", sortType = "desc", userId } = req.query;
@@ -93,3 +94,58 @@ export const getAllVideos = asyncHandler(async (req, res) => {
         }, "Videos fetched successfully")
     );
 });
+
+export const publishVideo = asyncHandler(async(req,res)=>{
+    const {title, description} = req.body
+
+    if([title,description].some((field) => field?.trim()=== "")){
+        throw new ApiError(400, "Title and description is required")
+    }
+
+    if(!req.files?.videoFile || !req.files?.thumbnail){
+        throw new ApiError(400, "video file and thumbnail is required")
+    }
+
+    let videoFileUrl;
+
+    if(req.files && Array.isArray(req.files.videoFile) && req.files.videoFile.length>0){
+        videoFileUrl = req.files.videoFile[0].path;
+    }
+    if(!videoFileUrl){
+        throw new ApiError(400,"video file is required")
+    }
+
+    let thumbnailUrl;
+    if(req.files && Array.isArray(req.files.thumbnail) && req.files.thumbnail.length>0){
+        thumbnailUrl = req.files.thumbnail[0].path;
+    }
+
+    if(!thumbnailUrl){
+        throw new ApiError(400,"thumbnail is required")
+    }
+
+    const videoFileUploadResult = await uploadOnCloudinary(videoFileUrl)
+
+    const thumbnailUploadResult = await uploadOnCloudinary(thumbnailUrl)
+
+    if(!videoFileUploadResult || !thumbnailUploadResult){
+        throw new ApiError(400, "failed to upload video or thumbnail")
+    }
+
+    console.log(videoFileUploadResult)
+
+    const video = await Video.create({
+        title: title,
+        description: description,
+        videoFile: videoFileUploadResult.url,
+        thumbnail: thumbnailUploadResult.url,
+        owner: req.user._id,
+        isPublished: true,
+        duration:videoFileUploadResult.duration,
+        views: 0
+    })
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, video, "video published successfully"))
+})
