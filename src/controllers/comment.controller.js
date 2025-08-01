cc78e1d228de593482e3762970e116a74a40423f
unknown <khushisingh74003@gmail.com>
2025-08-01 17:03:11 +0530

    added delete comment

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -101,3 +101,33 @@ export const updateComment = asyncHandler(async(req,res) =>{
     return res.status(200)
     .json(new ApiResponse(200, comment, "comment updated successfully"))
 })
+
+
+export const deleteComment = asyncHandler(async(req, res) => {
+    const { commentId } = req.params;
+
+    if (!commentId?.trim()) {
+        throw new ApiError(400, "Comment ID is required");
+    }
+
+    if (!mongoose.isValidObjectId(commentId)) {
+        throw new ApiError(400, "Invalid comment ID");
+    }
+
+    const comment = await Comment.findById(commentId);
+
+    if (!comment) {
+        throw new ApiError(404, "Comment not found");
+    }
+
+    
+    if (String(comment.owner) !== String(req.user._id)) {
+        throw new ApiError(403, "You can only delete your own comments");
+    }
+
+    await comment.deleteOne();
+
+    return res.status(200).json(
+        new ApiResponse(200, comment, "Comment deleted successfully")
+    );
+});
