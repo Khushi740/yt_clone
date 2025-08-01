27c517906e822d268b6ae33a8a4fca626268a4aa
unknown <khushisingh74003@gmail.com>
2025-08-01 16:59:00 +0530

    added updateComment

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -47,3 +47,57 @@ export const getVideoComments = asyncHandler(async(req,res) =>{
 
     
 })
+
+export const addComment = asyncHandler(async(req,res) =>{
+   const {videoId} =req.params;
+   const {content} = req.body;
+
+   if(!videoId?.trim()){
+         throw new ApiError(400,"video id is required")
+   }
+    if(!mongoose.isValidObjectId(videoId)){
+        throw new ApiError(400,"video id is not valid")
+    }
+
+    if(!content?.trim()){
+        throw new ApiError(400,"content is required")
+    }
+
+    const comment = await Comment.create({
+        content: content,
+        video: videoId,
+        owner: req.user._id
+    })
+
+    if(!comment){
+        throw new ApiError(500,"failed to add comment")
+    }
+
+    return res.status(201).json(new ApiResponse(201, comment, "comment added successfully"))
+
+})
+
+export const updateComment = asyncHandler(async(req,res) =>{
+    const {commentId} =req.params;
+    const {content} = req.body;
+    if(!commentId?.trim()){
+        throw new ApiError(400,"comment id is required") 
+    }
+
+    if(!content?.trim()){
+        throw new ApiError(400,"content is required")
+    }
+
+    const comment = await Comment.findByIdAndUpdate(
+        commentId,
+         { content }, 
+         { new: true ,
+             runValidators: true
+         });
+
+    if(!comment){
+        throw new ApiError(404,"comment not found")
+    }
+    return res.status(200)
+    .json(new ApiResponse(200, comment, "comment updated successfully"))
+})
